3aa0fdeaa010df84d619199f6ed45476cd522005
HirotakaUchishiba <hirotaka1999821@gmail.com>
2023-04-13 16:04:42 +0900

    [Add] randが0の場合、おみくじの結果を「大吉」とするように変更。

--- omikuji.js
+++ omikuji.js
@@ -4,3 +4,7 @@ username = prompt("お名前を教えて下さい。");
 document.getElementById("name").innerHTML = username;
 
 let rand = Math.floor( Math.random() * 5) ;
+
+if (rand == 0) {
+    userresult = "大吉";
+  }
